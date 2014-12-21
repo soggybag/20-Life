2a0baa8f5acd13ea3c52466a7d7fd2d34d79eb08
Mitchell Hudson <soggybag@gmail.com>
2014-12-20 23:02:57 -0800

    Initial Commit

---
+++ Life Counter 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Life Counter 3
+//
+//  Created by mitchell hudson on 12/20/14.
+//  Copyright (c) 2014 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
